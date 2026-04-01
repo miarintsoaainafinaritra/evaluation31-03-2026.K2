7b234a77e0a0c1f6cf887c56ca7fdfe1487a7e32
Miarintsoa Christian <ainafinaritra17@gmail.com>
2026-04-01 10:05:21 +0300

    Remove @Configuration annotation from DBConnection
    Removed the @Configuration annotation from DBConnection class.

--- src/main/java/com/restaurant/config/DBConnection.java
+++ src/main/java/com/restaurant/config/DBConnection.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
-//@Configuration
 public class DBConnection {
 
     @Bean
